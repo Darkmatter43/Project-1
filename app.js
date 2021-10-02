a57b89301943af3a6ef8d5387e68cacecfbf17d0
Yianni Catege <ycatege@gmail.com>
2021-10-02 13:59:10 -0400

    pulled an image to test styling options

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-$(()=>{
+let userMon = 'charmander'
+const generateMon =(data)=>{
+    // const $newMon = $()
+    const monImg = data.sprites.other['official-artwork'].front_default
+    console.log(data.sprites.other['official-artwork'].front_default);
 
+    $('.test').append($('<img>').attr('src',monImg))
+}
+$(()=>{
+    
 
 $.ajax({
-    url: 'https://pokeapi.co/api/v2/pokemon/'
+    url: `https://pokeapi.co/api/v2/pokemon/${userMon.toLowerCase()}`
 }).then(
     (data)=>{
         console.log(data);
+        generateMon(data)
+    },
+    ()=>{
+        console.log('bad request');
 })
-
 })
