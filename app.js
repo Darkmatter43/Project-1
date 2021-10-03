22737958a608d42440a439ba575542e2c2fa448e
Yianni Catege <ycatege@gmail.com>
2021-10-03 18:30:34 -0400

    generation buttons functional! Create pokemon and empty the old ones.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let userMon = 'charmander'
-//function that creates a card of the image of the pokemon as well as the small info panel below it\
+//function that creates a card of the image of the pokemon as well as the small info panel below it - data is the pokemon object
 const generateMon =(data)=>{
     // const $newMon = $()
     const monImg = data.sprites.other['official-artwork'].front_default
@@ -39,6 +39,142 @@ $(()=>{
         })
     })
 
+    $('input[value="Gen I"').on('click',(e)=>{
+        $('.pokemon').empty()
+        //tried making a for loop that pulls a single mon from the API every iteration, but they don't show up in order. Not sure why. Conisdering other options...
+        for (i=1;i<=151;i++){
+            $.ajax({
+                url: `https://pokeapi.co/api/v2/pokemon/${i}`
+            }).then(
+                (data)=>{
+                    generateMon(data)
+                    // console.log(Object.keys(data));
+                    // generateMon(data.results[0].url)
+                },
+                ()=>{
+                    console.log('bad request');
+            })
+        }
+    })
+    $('input[value="Gen II"').on('click',(e)=>{
+        $('.pokemon').empty()
+        //tried making a for loop that pulls a single mon from the API every iteration, but they don't show up in order. Not sure why. Conisdering other options...
+        for (i=152;i<=251;i++){
+            $.ajax({
+                url: `https://pokeapi.co/api/v2/pokemon/${i}`
+            }).then(
+                (data)=>{
+                    generateMon(data)
+                    // console.log(Object.keys(data));
+                    // generateMon(data.results[0].url)
+                },
+                ()=>{
+                    console.log('bad request');
+            })
+        }
+    })
+    $('input[value="Gen III"').on('click',(e)=>{
+        $('.pokemon').empty()
+        //tried making a for loop that pulls a single mon from the API every iteration, but they don't show up in order. Not sure why. Conisdering other options...
+        for (i=252;i<=386;i++){
+            $.ajax({
+                url: `https://pokeapi.co/api/v2/pokemon/${i}`
+            }).then(
+                (data)=>{
+                    generateMon(data)
+                    // console.log(Object.keys(data));
+                    // generateMon(data.results[0].url)
+                },
+                ()=>{
+                    console.log('bad request');
+            })
+        }
+    })
+    $('input[value="Gen IV"').on('click',(e)=>{
+        $('.pokemon').empty()
+        //tried making a for loop that pulls a single mon from the API every iteration, but they don't show up in order. Not sure why. Conisdering other options...
+        for (i=387;i<=494;i++){
+            $.ajax({
+                url: `https://pokeapi.co/api/v2/pokemon/${i}`
+            }).then(
+                (data)=>{
+                    generateMon(data)
+                    // console.log(Object.keys(data));
+                    // generateMon(data.results[0].url)
+                },
+                ()=>{
+                    console.log('bad request');
+            })
+        }
+    })
+    $('input[value="Gen V"').on('click',(e)=>{
+        $('.pokemon').empty()
+        //tried making a for loop that pulls a single mon from the API every iteration, but they don't show up in order. Not sure why. Conisdering other options...
+        for (i=495;i<=649;i++){
+            $.ajax({
+                url: `https://pokeapi.co/api/v2/pokemon/${i}`
+            }).then(
+                (data)=>{
+                    generateMon(data)
+                    // console.log(Object.keys(data));
+                    // generateMon(data.results[0].url)
+                },
+                ()=>{
+                    console.log('bad request');
+            })
+        }
+    })
+    $('input[value="Gen VI"').on('click',(e)=>{
+        $('.pokemon').empty()
+        //tried making a for loop that pulls a single mon from the API every iteration, but they don't show up in order. Not sure why. Conisdering other options...
+        for (i=650;i<=721;i++){
+            $.ajax({
+                url: `https://pokeapi.co/api/v2/pokemon/${i}`
+            }).then(
+                (data)=>{
+                    generateMon(data)
+                    // console.log(Object.keys(data));
+                    // generateMon(data.results[0].url)
+                },
+                ()=>{
+                    console.log('bad request');
+            })
+        }
+    })
+    $('input[value="Gen VII"').on('click',(e)=>{
+        $('.pokemon').empty()
+        //tried making a for loop that pulls a single mon from the API every iteration, but they don't show up in order. Not sure why. Conisdering other options...
+        for (i=722;i<=809;i++){
+            $.ajax({
+                url: `https://pokeapi.co/api/v2/pokemon/${i}`
+            }).then(
+                (data)=>{
+                    generateMon(data)
+                    // console.log(Object.keys(data));
+                    // generateMon(data.results[0].url)
+                },
+                ()=>{
+                    console.log('bad request');
+            })
+        }
+    })
+    $('input[value="Gen VIII"').on('click',(e)=>{
+        $('.pokemon').empty()
+        //tried making a for loop that pulls a single mon from the API every iteration, but they don't show up in order. Not sure why. Conisdering other options...
+        for (i=810;i<=898;i++){
+            $.ajax({
+                url: `https://pokeapi.co/api/v2/pokemon/${i}`
+            }).then(
+                (data)=>{
+                    generateMon(data)
+                    // console.log(Object.keys(data));
+                    // generateMon(data.results[0].url)
+                },
+                ()=>{
+                    console.log('bad request');
+            })
+        }
+    })
 // $.ajax({
     
 //     url: `https://pokeapi.co/api/v2/pokemon/${userMon.toLowerCase()}`
