ceceae2600efbf2d525f41d6a29c1eeabd87f602
Yianni Catege <ycatege@gmail.com>
2021-10-03 13:35:18 -0400

    search bar adds a pokemon to the page! nice.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let userMon = 'charmander'
 //function that creates a card of the image of the pokemon as well as the small info panel below it\
 const generateMon =(data)=>{
     // const $newMon = $()
+   
     const monImg = data.sprites.other['official-artwork'].front_default
 
     const $newCard = ($('<div>')).attr('class','card').append($('<img>').attr('src',monImg))
@@ -12,22 +13,43 @@ const generateMon =(data)=>{
 
     $('<h1>').text(data.id+':').appendTo($infoDiv)
     $('<h1>').text(data.name).appendTo($infoDiv)
+
+    
 }
 $(()=>{
+    $('form').on('submit',(e)=>{
+        e.preventDefault()
+        userMon = $('input[type="text"]').val()
+        $.ajax({
     
+            url: `https://pokeapi.co/api/v2/pokemon/${userMon.toLowerCase()}`
+            // url: `https://pokeapi.co/api/v2/pokemon/?limit=897`
+            // url: `https://pokeapi.co/api/v2/pokedex/2`
+        }).then(
+            (data)=>{
+                // console.log(data.pokemon_entries[5].pokemon_species.name);
+                generateMon(data)
+                console.log(data);
+            },
+            ()=>{
+                console.log('bad request');
+        })
+        // generateMon()
+        
+    })
 
-$.ajax({
+// $.ajax({
     
-    url: `https://pokeapi.co/api/v2/pokemon/${userMon.toLowerCase()}`
-    // url: `https://pokeapi.co/api/v2/pokemon/?limit=897`
-    // url: `https://pokeapi.co/api/v2/pokedex/2`
-}).then(
-    (data)=>{
-        // console.log(data.pokemon_entries[5].pokemon_species.name);
-        generateMon(data)
-        console.log(data);
-    },
-    ()=>{
-        console.log('bad request');
-})
+//     url: `https://pokeapi.co/api/v2/pokemon/${userMon.toLowerCase()}`
+//     // url: `https://pokeapi.co/api/v2/pokemon/?limit=897`
+//     // url: `https://pokeapi.co/api/v2/pokedex/2`
+// }).then(
+//     (data)=>{
+//         // console.log(data.pokemon_entries[5].pokemon_species.name);
+//         // generateMon(data)
+//         console.log(data);
+//     },
+//     ()=>{
+//         console.log('bad request');
+// })
 })
